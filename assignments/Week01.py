82f89ae93ca9a6729f604cf7226dfc00aadd2640
Krittipong-Chaiphinitnun <krittipong.c@ku.th>
2025-07-08 15:36:29 +0700

    Week01

---
+++ assignments/Week01.py
@@ -0,0 +1,23 @@
+# Exercise 1: Personal Profile Creator
+print("=== Personal Profile Creator ===")
+# Create a program that asks for:
+# - Full name
+# - Age
+# - Email
+# - Phone number
+# - Favorite hobby
+# Then display it as a profile
+
+# Write your solution here:
+
+Full_name = input("What's your name? :")
+Age = input("How old are you? :")
+Email = input("What is your email? :")
+Phone_number = input("What is your phone number?" :)
+Hobby = input("What is your favorite hobby? :")
+
+print("== Your profile ==")
+print("Full name:" + Full_name)
+print("Age: ", Age)print("Email: ", Email)
+print("Phone no: " Phone_number)
+print("Favorite hobby: " Hobby)
